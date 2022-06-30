fd3203288cf03626475bd40aa2f9af2e728364c9
MiloCOLO <100871599+MiloCOLO@users.noreply.github.com>
2022-06-30 17:51:02 -0600

    linked local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,23 @@
 var today = moment();
 
-$("#currentDay").text(today.format("MMM Do, YYYY"))
+$("#currentDay").text(today.format("MMM Do, YYYY")) //include day of the week 
 
+function saveBtn(saveInfo) {
+    console.log(saveInfo.target.id)
+}
+
+var button = document.querySelectorAll(".saveBtn")
+console.log(button)
+for (let index = 0; index < button.length; index++) {
+    const element = button[index];
+    element.addEventListener("click", saveBtn)
+}
+
+{
+    key: "information"
+}
+function keys(Arr) {
+    var strings = JSON.stringify(Arr)
+    localStorage.setItem("information", strings)
+
+}
