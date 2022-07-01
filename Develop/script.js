aa94bc666b4b8b36b0f707dc9e02457139e0d25a
MiloCOLO <100871599+MiloCOLO@users.noreply.github.com>
2022-06-30 21:28:19 -0600

    minor code revisions

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,25 @@
 var today = moment();
 
-$("#currentDay").text(today.format("MMM Do, YYYY")) //include day of the week 
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY")) //displays current time including day of the week 
+
+const
+
+
+
+
+
+
+
 
 function saveBtn(saveInfo) {
     console.log(saveInfo.target.id)
+    var save = $(saveInfo.target.id)
+    console.log(save)
+    saveSiblings = save.siblings().val()
+    console.log(saveSiblings)
 }
 
-var button = document.querySelectorAll(".saveBtn")
+var button = document.querySelectorAll(".saveBtn") //makes the save button clickable 
 console.log(button)
 for (let index = 0; index < button.length; index++) {
     const element = button[index];
@@ -19,5 +32,18 @@ for (let index = 0; index < button.length; index++) {
 function keys(Arr) {
     var strings = JSON.stringify(Arr)
     localStorage.setItem("information", strings)
-
+    return 
 }
+
+
+function timeSlot(before, after) {
+    if (hour == before) {
+        after.addClass("present");
+    }   else if (hour < before) {
+        after.addClass("future");
+    }   else if (hour > before) {
+        after.addClass("past")
+    }
+}
+
+timeSlot;
